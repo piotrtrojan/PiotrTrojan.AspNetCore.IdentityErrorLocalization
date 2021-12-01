92ac1f0d31706b10268439e61361b0e0e1c86ef7
Carlos Gomes <cacpgomes@gmail.com>
2021-12-01 23:24:41 +0000

    Update PortugueseIdentityErrorDescriber.cs
    I'm a Portuguese developer and I adapt the descriptions to Portuguese from Portugal (not Brazil)

--- source/PiotrTrojan.AspNetCore.IdentityErrorLocalization.pt-PT/PortugueseIdentityErrorDescriber.cs
+++ source/PiotrTrojan.AspNetCore.IdentityErrorLocalization.pt-PT/PortugueseIdentityErrorDescriber.cs
@@ -4,25 +4,25 @@ namespace PiotrTrojan.AspNetCore.IdentityErrorLocalization.pt_PT
 {
     public class PortugueseIdentityErrorDescriber : IdentityErrorDescriber
     {
-        public override IdentityError ConcurrencyFailure() => new() { Code = nameof(ConcurrencyFailure), Description = "Falha de concorrência otimista, o objeto foi modificado." };
-        public override IdentityError DefaultError() => new() { Code = nameof(DefaultError), Description = $"Um erro desconhecido ocorreu." };
-        public override IdentityError DuplicateEmail(string email) => new() { Code = nameof(DuplicateEmail), Description = $"Email '{email}' já está sendo utilizado." };
+        public override IdentityError ConcurrencyFailure() => new() { Code = nameof(ConcurrencyFailure), Description = "Falha de concorrência, o objeto foi modificado." };
+        public override IdentityError DefaultError() => new() { Code = nameof(DefaultError), Description = $"Ocorreu um erro desconhecido." };
+        public override IdentityError DuplicateEmail(string email) => new() { Code = nameof(DuplicateEmail), Description = $"O email '{email}' já está sendo utilizado." };
         public override IdentityError DuplicateRoleName(string role) => new() { Code = nameof(DuplicateRoleName), Description = $"A permissão '{role}' já está sendo utilizada." };
-        public override IdentityError DuplicateUserName(string userName) => new() { Code = nameof(DuplicateUserName), Description = $"Login '{userName}' já está sendo utilizado." };
-        public override IdentityError InvalidEmail(string email) => new() { Code = nameof(InvalidEmail), Description = $"Email '{email}' é inválido." };
+        public override IdentityError DuplicateUserName(string userName) => new() { Code = nameof(DuplicateUserName), Description = $"O utilizador '{userName}' já está sendo utilizado." };
+        public override IdentityError InvalidEmail(string email) => new() { Code = nameof(InvalidEmail), Description = $"O email '{email}' é inválido." };
         public override IdentityError InvalidRoleName(string role) => new() { Code = nameof(InvalidRoleName), Description = $"A permissão '{role}' é inválida." };
         public override IdentityError InvalidToken() => new() { Code = nameof(InvalidToken), Description = "Token inválido." };
-        public override IdentityError InvalidUserName(string userName) => new() { Code = nameof(InvalidUserName), Description = $"Login '{userName}' é inválido, pode conter apenas letras ou dígitos." };
-        public override IdentityError LoginAlreadyAssociated() => new() { Code = nameof(LoginAlreadyAssociated), Description = "Já existe um usuário com este login." };
+        public override IdentityError InvalidUserName(string userName) => new() { Code = nameof(InvalidUserName), Description = $"O utilizador '{userName}' é inválido. Só pode conter apenas letras ou dígitos." };
+        public override IdentityError LoginAlreadyAssociated() => new() { Code = nameof(LoginAlreadyAssociated), Description = "Já existe um utilizador com este login." };
         public override IdentityError PasswordMismatch() => new() { Code = nameof(PasswordMismatch), Description = "Senha incorreta." };
-        public override IdentityError PasswordRequiresDigit() => new() { Code = nameof(PasswordRequiresDigit), Description = "Senhas devem conter ao menos um digito ('0'-'9')." };
-        public override IdentityError PasswordRequiresLower() => new() { Code = nameof(PasswordRequiresLower), Description = "Senhas devem conter ao menos um caracter em caixa baixa ('a'-'z')." };
-        public override IdentityError PasswordRequiresNonAlphanumeric() => new() { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "Senhas devem conter ao menos um caracter não alfanumérico." };
-        public override IdentityError PasswordRequiresUpper() => new() { Code = nameof(PasswordRequiresUpper), Description = "Senhas devem conter ao menos um caracter em caixa alta ('A'-'Z')." };
-        public override IdentityError PasswordTooShort(int length) => new() { Code = nameof(PasswordTooShort), Description = $"Senhas devem conter ao menos {length} caracteres." };
-        public override IdentityError UserAlreadyHasPassword() => new() { Code = nameof(UserAlreadyHasPassword), Description = "Usuário já possui uma senha definida." };
-        public override IdentityError UserAlreadyInRole(string role) => new() { Code = nameof(UserAlreadyInRole), Description = $"Usuário já possui a permissão '{role}'." };
-        public override IdentityError UserLockoutNotEnabled() => new() { Code = nameof(UserLockoutNotEnabled), Description = "Lockout não está habilitado para este usuário." };
-        public override IdentityError UserNotInRole(string role) => new() { Code = nameof(UserNotInRole), Description = $"Usuário não tem a permissão '{role}'." };
+        public override IdentityError PasswordRequiresDigit() => new() { Code = nameof(PasswordRequiresDigit), Description = "As senhas devem conter pelo menos um dígito ('0'-'9')." };
+        public override IdentityError PasswordRequiresLower() => new() { Code = nameof(PasswordRequiresLower), Description = "As senhas devem conter pelo menos um caracter em minúscula ('a'-'z')." };
+        public override IdentityError PasswordRequiresNonAlphanumeric() => new() { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "As senhas devem conter pelo menos um caracter não alfanumérico." };
+        public override IdentityError PasswordRequiresUpper() => new() { Code = nameof(PasswordRequiresUpper), Description = "As senhas devem conter pelo menos um caracter em maiúscula ('A'-'Z')." };
+        public override IdentityError PasswordTooShort(int length) => new() { Code = nameof(PasswordTooShort), Description = $"As senhas devem conter pelo menos {length} caracteres." };
+        public override IdentityError UserAlreadyHasPassword() => new() { Code = nameof(UserAlreadyHasPassword), Description = "O utilizador já possui uma senha definida." };
+        public override IdentityError UserAlreadyInRole(string role) => new() { Code = nameof(UserAlreadyInRole), Description = $"O utilizador já possui a permissão '{role}'." };
+        public override IdentityError UserLockoutNotEnabled() => new() { Code = nameof(UserLockoutNotEnabled), Description = "Lockout não está disponível para este utilizador." };
+        public override IdentityError UserNotInRole(string role) => new() { Code = nameof(UserNotInRole), Description = $"O utilizador não tem a permissão '{role}'." };
     }
 }
